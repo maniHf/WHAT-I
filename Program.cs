6d0f6611928716daf218594acc6ca2fb81e173ee
OLDSPYDERMAN <112350245+maniHf@users.noreply.github.com>
2023-10-21 23:28:30 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿class
+        C1
+{
+    static 
+            void Main
+        ()
+    {
+
+        string[]         args =
+            Environment.
+                            GetCommandLineArgs
+                            ();
+        foreach (
+   string arg 
+                                        in args)
+        {
+            Console.
+                            WriteLine
+                (arg);
+        }
+        int i = 100;
+        for (i = 100; i < 200; i++) Console.WriteLine ("I have problems with my head\n");
+        Console.ReadLine
+            ();
+
+    }
+}
